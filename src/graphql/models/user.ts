80825e435ac0734766ba0179cbc6ea6d32590a1a
Bharath Kesineni <bharath.kesineni@brainvire.com>
2024-11-08 19:06:16 +0530

    updated graphql models resolvers and db connection

---
+++ src/graphql/models/user.ts
@@ -0,0 +1,30 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { UserSetting } from './userSetting';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity({ name: 'users' })
+@ObjectType()
+export class User {
+  @PrimaryGeneratedColumn()
+  @Field((type) => Int)
+  id: number;
+
+  @Column()
+  @Field()
+  username: string;
+
+  @Column()
+  @Field({ nullable: true })
+  displayName?: string;
+
+  @OneToOne(() => UserSetting)
+  @JoinColumn()
+  @Field({ nullable: true })
+  settings?: UserSetting;
+}
